123d90c74173b444381568655b412c01527e6686
Paul Esthappan <135216650+Paul-Esthappan@users.noreply.github.com>
2024-04-26 12:47:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Projects from './components/Projects';
 import Resuma from './components/Resuma';
 import Topnav from './components/Topnav'
 import Work from './components/Work';
-import Main from "./components/main";
+import Main from "./components/Main";
 
 function App() {
   return (
