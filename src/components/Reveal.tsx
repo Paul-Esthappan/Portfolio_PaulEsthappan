94f51426dd506e3bf5cf7d9cf082db6c041ee52e
Paul-Esthappan <paulesthappan@gmail.com>
2026-03-11 20:51:19 +0530

    fix typescript eslint error

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import { motion } from "framer-motion"
+import { ReactNode } from "react"
 
-export default function Reveal({ children }: any) {
+type RevealProps = {
+  children: ReactNode
+}
+
+export default function Reveal({ children }: RevealProps) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
